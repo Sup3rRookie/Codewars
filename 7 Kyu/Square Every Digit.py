dc832f23ca61cb1a9b1d9781378177d07b3673c9
Sup3rRookie <dev.akaun@gmail.com>
2021-09-16 12:43:07 +0800

    Update Square Every Digit.py

--- 7 Kyu/Square Every Digit.py
+++ 7 Kyu/Square Every Digit.py
@@ -1,4 +1,5 @@
 def square_digits(num):
+    # Solution 1
     n =  [int(i) for i in str(num)]
     result = [i*i for i in list(map(int, n))]
     strings = [str(integer) for integer in result]
@@ -6,4 +7,9 @@ def square_digits(num):
     an_integer = int(a_string)
     return an_integer
 
+    # # Solution 2
+    # ret = ""
+    # for x in str(num):
+    #     ret += str(int(x)**2)
+    # return int(ret)
 square_digits(9119)
